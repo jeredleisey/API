4be404ce12caa3d0c2a4dcf466bd123fe2db85b3
icarus <jered.leisey@gmail.com>
2020-12-03 15:31:46 -0700

    created server

---
+++ source/server.ts
@@ -0,0 +1,50 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import logging from './config/logging';
+import config from './config/config';
+
+const NAMESPACE = 'Server';
+const router = express();
+
+/** Loggging the request */
+router.use((req, res, next) => {
+    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        logging.info(
+            NAMESPACE,
+            `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}],
+         STATUS - [${res.statusCode}]`
+        );
+    });
+});
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+/** Rules of the API */
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET PATH DELETE POST PUT');
+        return res.status(200).json({});
+    }
+});
+
+/** Routes */
+
+/** Error Handling */
+router.use((req, res, next) => {
+    const error = new Error('not found');
+
+    return res.status(404).json({
+        message: error.message
+    });
+});
+
+/** Create the server */
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.port}`));
